6733552e108916b5636c26da9f2baa01508697dd
Alec Faustino <alecfaustino@outlook.com>
2025-07-27 12:30:43 -0600

    install and add cors middleware to server.ts

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+const cors = require("cors");
+app.use(cors());
 const morgan = require("morgan");
 app.use(express.json());
 app.use(morgan("dev"));
