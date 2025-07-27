23c14fe8b86dd78c1a5bc0a7bd177da63e51b2c6
Alec Faustino <alecfaustino@outlook.com>
2025-07-26 18:22:28 -0600

    config nodemon

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,18 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = 8080;
 
+const morgan = require("morgan");
+app.use(express.json());
+app.use(morgan("dev"));
+
+// router definition
+const spoonRoutes = require("./routes/spoon-api");
+
+// router mounting
+app.use("/api/spoon", spoonRoutes);
+
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
