14ecd1652c87d53cbe247db06590b90d5998e992
Alec Faustino <alecfaustino@outlook.com>
2025-07-27 13:33:33 -0600

    render the food descriptions on the page

--- src/pages/Potato.tsx
+++ src/pages/Potato.tsx
@@ -1,9 +1,35 @@
 import axios, {AxiosError} from 'axios';
 import { useState } from 'react';
+type Food = {
+  description: string;
+}
 const Potato = () => {
+  const [foods, setFoods] = useState<Food[]>([]);
+
+  const fetchInfo = async () => {
+    try {
+      const fetchResult = await axios.get(`http://localhost:8080/api/usda/search`)
+      // fetchResult.data.data.foods is the array of foods
+      setFoods(fetchResult.data.data.foods);
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        console.log("temporary code");
+      }
+    }
+  }
   return (
     <>
       <h1>Hello From Potato</h1>
+
+      <button onClick={fetchInfo}>Fetch Info</button>
+      <div>
+        {foods.map((food, index) => (
+          <div key={index}>
+            <h2>Name</h2>
+            <p>{food.description}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
