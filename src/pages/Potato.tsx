2d52667c6f647f29ec132d7ea778824359c93124
Alec Faustino <alecfaustino@outlook.com>
2025-07-27 12:26:39 -0600

    init Potato page and mount it to App.tsx

---
+++ src/pages/Potato.tsx
@@ -0,0 +1,11 @@
+import axios, {AxiosError} from 'axios';
+import { useState } from 'react';
+const Potato = () => {
+  return (
+    <>
+      <h1>Hello From Potato</h1>
+    </>
+  )
+}
+
+export default Potato
