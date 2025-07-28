effcbbea061f0ca93d71b8ed2b5a12736d83dbda
Alec Faustino <alecfaustino@outlook.com>
2025-07-27 20:22:13 -0600

    render the calory value for each food

--- src/pages/Potato.tsx
+++ src/pages/Potato.tsx
@@ -1,16 +1,28 @@
 import axios, {AxiosError} from 'axios';
 import { useState } from 'react';
+
 type Food = {
+  fdcId: number;
   description: string;
-}
+  foodNutrients: Nutrient[];
+};
+
+type Nutrient = {
+  nutrientName: string;
+  value: number;
+  unitName: string;
+};
+
 const Potato = () => {
   const [foods, setFoods] = useState<Food[]>([]);
-
+  
   const fetchInfo = async () => {
     try {
       const fetchResult = await axios.get(`http://localhost:8080/api/usda/search`)
       // fetchResult.data.data.foods is the array of foods
-      setFoods(fetchResult.data.data.foods);
+      const foodList: Food[] = fetchResult.data.data.foods;
+      // TODO make a single useState hook with both the calories and description to loop through it
+      setFoods(foodList);
     } catch (error) {
       if (axios.isAxiosError(error) && error.response?.status === 401) {
         console.log("temporary code");
@@ -25,8 +37,10 @@ const Potato = () => {
       <div>
         {foods.map((food, index) => (
           <div key={index}>
-            <h2>Name</h2>
+            <p>Name:</p>
             <p>{food.description}</p>
+            <p>Calories:</p>
+            <p>{food.foodNutrients[3].value} {food.foodNutrients[3].unitName}</p>
           </div>
         ))}
       </div>
