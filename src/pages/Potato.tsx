86ba21702c4c8da8a0d085743e1072e5c3fb34c6
Alec Faustino <alecfaustino@outlook.com>
2025-07-28 11:27:20 -0600

    Render the potato search into a view (#4)
    - hard coded search of potato.
    - Rendered description and calories into the page.
    - Purpose is to start communication between front end and back end and set up environment variables for front end

---
+++ src/pages/Potato.tsx
@@ -0,0 +1,60 @@
+import axios, {AxiosError} from 'axios';
+import { useState } from 'react';
+
+type Food = {
+  fdcId: number;
+  description: string;
+  foodNutrients: Nutrient[];
+};
+
+type Nutrient = {
+  nutrientName: string;
+  value: number;
+  unitName: string;
+};
+
+const Potato = () => {
+  const [foods, setFoods] = useState<Food[]>([]);
+  const baseUrl = import.meta.env.VITE_API_BASE_URL;
+  
+  const fetchInfo = async () => {
+    try {
+      console.log("Button clicked!");
+      const fetchResult = await axios.get(`${baseUrl}/api/usda/search`)
+      // fetchResult.data.data.foods is the array of foods
+      // if not data is returned, foodList = [];
+      const foodList: Food[] = fetchResult?.data?.data?.foods ?? [];
+      // TODO make a single useState hook with both the calories and description to loop through it
+      setFoods(foodList);
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response?.status === 401) {
+        console.log("temporary code");
+      }
+    }
+  }
+  return (
+    <>
+      <h1>Hello From Potato</h1>
+
+      <button onClick={fetchInfo}>Fetch Info</button>
+      <div>
+        {foods.map((food, index) => {
+          const energy = food.foodNutrients.find((nutrient) => nutrient.nutrientName === "Energy");
+
+          return (
+          <div key={index}>
+            <p>Name:</p>
+            <p>{food.description}</p>
+            <p>Calories:</p>
+            <p>{energy? `${energy.value} ${energy.unitName}` : "Calories not found"} 
+            </p>
+          </div>
+
+          );
+        })}
+      </div>
+    </>
+  )
+}
+
+export default Potato
